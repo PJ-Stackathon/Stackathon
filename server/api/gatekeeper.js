5f2f89857c6f7fdc590f7cd34c3651fd588fdc1d
Joanne <jl7105@nyu.edu>
2022-03-12 17:00:05 -0500

    added gatekeeper, GET user route, and singleUser redux

---
+++ server/api/gatekeeper.js
@@ -0,0 +1,38 @@
+const {
+  models: { User },
+} = require("../db");
+
+// const requireAdminToken = async (req, res, next) => {
+//   try {
+//     const token = req.headers.authorization;
+//     const user = await User.findByToken(token);
+//     if (user.type === "admin") {
+//       req.user = user;
+//       next();
+//     } else {
+//       return res.status(403).send("Sorry, no entry beyond this point!");
+//     }
+//     next();
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+const requireUserToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    console.log(">>>>>>>>>", token)
+    const user = await User.findByToken(token);
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+module.exports = {
+  requireUserToken,
+};
+
+// requireAdminToken
