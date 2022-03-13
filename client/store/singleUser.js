4c240688c18ac019f88b13e57fb7b6881bcd3113
Pamela Jung <72463342+pamify@users.noreply.github.com>
2022-03-13 01:49:28 -0500

    global configuration of prettier

--- client/store/singleUser.js
+++ client/store/singleUser.js
@@ -1,53 +1,49 @@
-import axios from 'axios';
+import axios from "axios";
 
 // ACTION TYPES:
-const SET_SINGLE_USER = 'SET_SINGLE_USER';
+const SET_SINGLE_USER = "SET_SINGLE_USER";
 
 // ACTION CREATORS:
-export const setSingleUser = (user) => ({
-  type: SET_SINGLE_USER,
-  user,
+export const setSingleUser = user => ({
+	type: SET_SINGLE_USER,
+	user
 });
 
 // THUNK CREATORS:
-export const fetchSingleUser = (userId) => {
-  return async (dispatch) => {
-    try {
-      const token = window.localStorage.getItem('token');
-      if (token) {
-        const { data } = await axios.get(`/api/users/${userId}`, {
-          headers: {
-            authorization: token,
-          },
-        });
-        dispatch(setSingleUser(data));
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+export const fetchSingleUser = userId => {
+	return async dispatch => {
+		try {
+			const token = window.localStorage.getItem("token");
+			if (token) {
+				const { data } = await axios.get(`/api/users/${userId}`, {
+					headers: {
+						authorization: token
+					}
+				});
+				dispatch(setSingleUser(data));
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 };
 
-export const updateUser = (updatedUser) => {
-  return async (dispatch) => {
-    try {
-      const token = window.localStorage.getItem('token');
-      if (token) {
-        const { data } = await axios.put(
-          `/api/users/${updatedUser.id}`,
-          updatedUser,
-          {
-            headers: {
-              authorization: token,
-            },
-          }
-        );
-        dispatch(setSingleUser(data));
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+export const updateUser = updatedUser => {
+	return async dispatch => {
+		try {
+			const token = window.localStorage.getItem("token");
+			if (token) {
+				const { data } = await axios.put(`/api/users/${updatedUser.id}`, updatedUser, {
+					headers: {
+						authorization: token
+					}
+				});
+				dispatch(setSingleUser(data));
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 };
 
 // INITIAL STATE:
@@ -55,10 +51,10 @@ const initialState = {};
 
 // SUB-REDUCER:
 export default function singleUserReducer(state = initialState, action) {
-  switch (action.type) {
-    case SET_SINGLE_USER:
-      return action.user;
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case SET_SINGLE_USER:
+			return action.user;
+		default:
+			return state;
+	}
 }
--- script/seed.js
+++ script/seed.js
@@ -1,7 +1,7 @@
 "use strict";
 const {
-  db,
-  models: { User, IdealMBTI },
+	db,
+	models: { User, IdealMBTI }
 } = require("../server/db");
 
 const surnameArray = require("./surnames.js");
@@ -9,35 +9,35 @@ const surnameArray = require("./surnames.js");
 const { uniqueNamesGenerator, names } = require("unique-names-generator");
 
 const mbtiArray = [
-  "INFP",
-  "ENFP",
-  "INFJ",
-  "ENFJ",
-  "INTJ",
-  "ENTJ",
-  "INTP",
-  "ENTP",
-  "ISFP",
-  "ESFP",
-  "ISTP",
-  "ESTP",
-  "ISFJ",
-  "ESFJ",
-  "ISTJ",
-  "ESTJ",
+	"INFP",
+	"ENFP",
+	"INFJ",
+	"ENFJ",
+	"INTJ",
+	"ENTJ",
+	"INTP",
+	"ENTP",
+	"ISFP",
+	"ESFP",
+	"ISTP",
+	"ESTP",
+	"ISFJ",
+	"ESFJ",
+	"ISTJ",
+	"ESTJ"
 ];
 
 const loveLanguageArray = [
-  "words-of-affirmation",
-  "quality-time",
-  "receiving-gifts",
-  "acts-of-service",
-  "physical-touch",
+	"words-of-affirmation",
+	"quality-time",
+	"receiving-gifts",
+	"acts-of-service",
+	"physical-touch"
 ];
 
 // find random single whole number:
 function pickRandom(list) {
-  return list[Math.floor(Math.random() * list.length)];
+	return list[Math.floor(Math.random() * list.length)];
 }
 
 /**
@@ -45,130 +45,130 @@ function pickRandom(list) {
  *      match the models, and populates the database.
  */
 async function seed() {
-  await db.sync({ force: true }); // clears db and matches models to tables
-  console.log("db synced!🌱");
+	await db.sync({ force: true }); // clears db and matches models to tables
+	console.log("db synced!🌱");
 
-  // Creating Users
-  const users = await Promise.all([
-    User.create({
-      username: "cody123",
-      firstName: "CoDy",
-      lastName: "ydoc",
-      password: "123",
-      email: "cody@seed.js",
-      mbti: "ENFJ",
-      loveLanguage: "words-of-affirmation",
-    }),
-    User.create({
-      username: "murphy123",
-      firstName: "MuRpHy",
-      lastName: "yhprum",
-      password: "123",
-      email: "murphy@seed.js",
-      mbti: "ISFP",
-      loveLanguage: "receiving-gifts",
-    }),
-  ]);
+	// Creating Users
+	const users = await Promise.all([
+		User.create({
+			username: "cody123",
+			firstName: "CoDy",
+			lastName: "ydoc",
+			password: "123",
+			email: "cody@seed.js",
+			mbti: "ENFJ",
+			loveLanguage: "words-of-affirmation"
+		}),
+		User.create({
+			username: "murphy123",
+			firstName: "MuRpHy",
+			lastName: "yhprum",
+			password: "123",
+			email: "murphy@seed.js",
+			mbti: "ISFP",
+			loveLanguage: "receiving-gifts"
+		})
+	]);
 
-  for (let i = 0; i < 50; i++) {
-    let randomNumComb = "";
+	for (let i = 0; i < 50; i++) {
+		let randomNumComb = "";
 
-    // for building usernames:
-    for (let j = 0; j < 3; j++) {
-      // Returns a random integer from 0 to 9:
-      let addNum = Math.floor(Math.random() * 10);
-      randomNumComb += addNum;
-    }
+		// for building usernames:
+		for (let j = 0; j < 3; j++) {
+			// Returns a random integer from 0 to 9:
+			let addNum = Math.floor(Math.random() * 10);
+			randomNumComb += addNum;
+		}
 
-    let firstName = uniqueNamesGenerator({ dictionaries: [names] });
-    let lastName = pickRandom(surnameArray);
-    let userFirst = firstName.toLowerCase()[0];
-    let userLast = lastName.toLowerCase().slice();
-    let username = userFirst + userLast + randomNumComb;
-    let email = username + "@seed.js";
-    let mbti = pickRandom(mbtiArray);
-    let loveLanguage = pickRandom(loveLanguageArray);
-    // let city = uniqueNamesGenerator({ dictionaries: [city] });
-    // let state = uniqueNamesGenerator({ dictionaries: [state] });
-    const users = await Promise.all([
-      User.create({
-        username: `${username}`,
-        password: "123",
-        email: `${email}`,
-        firstName: `${firstName}`,
-        lastName: `${lastName}`,
-        mbti: `${mbti}`,
-        loveLanguage: `${loveLanguage}`,
-        // city: `${city}`,
-        // state: `${state}`,
-      }),
-    ]);
-  }
+		let firstName = uniqueNamesGenerator({ dictionaries: [names] });
+		let lastName = pickRandom(surnameArray);
+		let userFirst = firstName.toLowerCase()[0];
+		let userLast = lastName.toLowerCase().slice();
+		let username = userFirst + userLast + randomNumComb;
+		let email = username + "@seed.js";
+		let mbti = pickRandom(mbtiArray);
+		let loveLanguage = pickRandom(loveLanguageArray);
+		// let city = uniqueNamesGenerator({ dictionaries: [city] });
+		// let state = uniqueNamesGenerator({ dictionaries: [state] });
+		const users = await Promise.all([
+			User.create({
+				username: `${username}`,
+				password: "123",
+				email: `${email}`,
+				firstName: `${firstName}`,
+				lastName: `${lastName}`,
+				mbti: `${mbti}`,
+				loveLanguage: `${loveLanguage}`
+				// city: `${city}`,
+				// state: `${state}`,
+			})
+		]);
+	}
 
-  IdealMBTI.bulkCreate([
-    { mbti: "INFP", idealMBTI: "ENFJ" },
-    { mbti: "INFP", idealMBTI: "ENTJ" },
+	IdealMBTI.bulkCreate([
+		{ mbti: "INFP", idealMBTI: "ENFJ" },
+		{ mbti: "INFP", idealMBTI: "ENTJ" },
 
-    { mbti: "ENFP", idealMBTI: "INFJ" },
-    { mbti: "ENFP", idealMBTI: "INTJ" },
+		{ mbti: "ENFP", idealMBTI: "INFJ" },
+		{ mbti: "ENFP", idealMBTI: "INTJ" },
 
-    { mbti: "INFJ", idealMBTI: "ENFP" },
-    { mbti: "INFJ", idealMBTI: "ENTP" },
+		{ mbti: "INFJ", idealMBTI: "ENFP" },
+		{ mbti: "INFJ", idealMBTI: "ENTP" },
 
-    { mbti: "ENFJ", idealMBTI: "INFP" },
-    { mbti: "ENFJ", idealMBTI: "ISFP" },
+		{ mbti: "ENFJ", idealMBTI: "INFP" },
+		{ mbti: "ENFJ", idealMBTI: "ISFP" },
 
-    { mbti: "INTJ", idealMBTI: "ENFP" },
-    { mbti: "INTJ", idealMBTI: "ENTP" },
+		{ mbti: "INTJ", idealMBTI: "ENFP" },
+		{ mbti: "INTJ", idealMBTI: "ENTP" },
 
-    { mbti: "ENTJ", idealMBTI: "INFP" },
-    { mbti: "ENTJ", idealMBTI: "INTP" },
+		{ mbti: "ENTJ", idealMBTI: "INFP" },
+		{ mbti: "ENTJ", idealMBTI: "INTP" },
 
-    { mbti: "INTP", idealMBTI: "ENTJ" },
-    { mbti: "INTP", idealMBTI: "ESTJ" },
+		{ mbti: "INTP", idealMBTI: "ENTJ" },
+		{ mbti: "INTP", idealMBTI: "ESTJ" },
 
-    { mbti: "ENTP", idealMBTI: "INFJ" },
-    { mbti: "ENTP", idealMBTI: "INTJ" },
+		{ mbti: "ENTP", idealMBTI: "INFJ" },
+		{ mbti: "ENTP", idealMBTI: "INTJ" },
 
-    { mbti: "ISFP", idealMBTI: "ENFJ" },
-    { mbti: "ISFP", idealMBTI: "ESFJ" },
-    { mbti: "ISFP", idealMBTI: "ESTJ" },
+		{ mbti: "ISFP", idealMBTI: "ENFJ" },
+		{ mbti: "ISFP", idealMBTI: "ESFJ" },
+		{ mbti: "ISFP", idealMBTI: "ESTJ" },
 
-    { mbti: "ESFP", idealMBTI: "ISFJ" },
-    { mbti: "ESFP", idealMBTI: "ISTJ" },
+		{ mbti: "ESFP", idealMBTI: "ISFJ" },
+		{ mbti: "ESFP", idealMBTI: "ISTJ" },
 
-    { mbti: "ISTP", idealMBTI: "ESFJ" },
-    { mbti: "ISTP", idealMBTI: "ESTJ" },
-    
-    { mbti: "ESTP", idealMBTI: "ISFJ" },
-    { mbti: "ESTP", idealMBTI: "ISTJ" },
+		{ mbti: "ISTP", idealMBTI: "ESFJ" },
+		{ mbti: "ISTP", idealMBTI: "ESTJ" },
 
-    { mbti: "ISFJ", idealMBTI: "ESFP" },
-    { mbti: "ISFJ", idealMBTI: "ESTP" },
+		{ mbti: "ESTP", idealMBTI: "ISFJ" },
+		{ mbti: "ESTP", idealMBTI: "ISTJ" },
 
-    { mbti: "ESFJ", idealMBTI: "ISFP" },
-    { mbti: "ESFJ", idealMBTI: "ISTP" },
+		{ mbti: "ISFJ", idealMBTI: "ESFP" },
+		{ mbti: "ISFJ", idealMBTI: "ESTP" },
 
-    { mbti: "ISTJ", idealMBTI: "ESFP" },
-    { mbti: "ISTJ", idealMBTI: "ESTP" },
+		{ mbti: "ESFJ", idealMBTI: "ISFP" },
+		{ mbti: "ESFJ", idealMBTI: "ISTP" },
 
-    { mbti: "ESTJ", idealMBTI: "INTP" },
-    { mbti: "ESTJ", idealMBTI: "ISFP" },
-    { mbti: "ESTJ", idealMBTI: "ISTP" },
-  ]);
+		{ mbti: "ISTJ", idealMBTI: "ESFP" },
+		{ mbti: "ISTJ", idealMBTI: "ESTP" },
 
-    const cody = User.findByPk(1);
-    const murphy = User.findByPk(2);
-    cody.addUser(murphy);
+		{ mbti: "ESTJ", idealMBTI: "INTP" },
+		{ mbti: "ESTJ", idealMBTI: "ISFP" },
+		{ mbti: "ESTJ", idealMBTI: "ISTP" }
+	]);
 
-  console.log(`seeded ${users.length} users`);
-  console.log(`seeded successfully`);
-  return {
-    users: {
-      cody: users[0],
-      murphy: users[1],
-    },
-  };
+	const cody = User.findByPk(1);
+	const murphy = User.findByPk(2);
+	cody.addUser(murphy);
+
+	console.log(`seeded ${users.length} users`);
+	console.log(`seeded successfully`);
+	return {
+		users: {
+			cody: users[0],
+			murphy: users[1]
+		}
+	};
 }
 
 /*
@@ -177,17 +177,17 @@ async function seed() {
  The `seed` function is concerned only with modifying the database.
 */
 async function runSeed() {
-  console.log("seeding...");
-  try {
-    await seed();
-  } catch (err) {
-    console.error(err);
-    process.exitCode = 1;
-  } finally {
-    console.log("closing db connection");
-    await db.close();
-    console.log("db connection closed");
-  }
+	console.log("seeding...");
+	try {
+		await seed();
+	} catch (err) {
+		console.error(err);
+		process.exitCode = 1;
+	} finally {
+		console.log("closing db connection");
+		await db.close();
+		console.log("db connection closed");
+	}
 }
 
 /*
@@ -196,7 +196,7 @@ async function runSeed() {
   any errors that might occur inside of `seed`.
 */
 if (module === require.main) {
-  runSeed();
+	runSeed();
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
