fba523bbf08bb9cf187ca75c3881f64d60b438cf
Joanne <jl7105@nyu.edu>
2022-03-14 03:42:40 -0400

    bottom navbar

---
+++ client/components/BottomNavbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+export const BottomNavbar = () => {
+	return (
+		<div className="bottom-navbar">
+			<ul className="desktop-list">
+				<li>
+					<div>
+						DISCOVER
+					</div>
+				</li>
+				<li>
+					<div>
+						LIKES YOU
+					</div>
+				</li>
+				<Link to="/users">
+					<li>
+						<div>
+							MATCHES
+						</div>
+					</li>
+				</Link>
+				<li>
+					<div>
+						SETTINGS
+					</div>
+				</li>
+			</ul>
+            <ul className="mobile-list">
+                <li><i className="ri-compass-discover-line"></i></li>
+                <li><i className="ri-heart-line"></i></li>
+                <li><i className="ri-chat-3-line"></i></li>
+                <li><i className="ri-user-line"></i></li>
+            </ul>
+		</div>
+	);
+};
+
+export default BottomNavbar;
--- server/api/gatekeeper.js
+++ server/api/gatekeeper.js
@@ -21,7 +21,6 @@ const {
 const requireUserToken = async (req, res, next) => {
 	try {
 		const token = req.headers.authorization;
-		console.log(">>>>>>>>>", token);
 		const user = await User.findByToken(token);
 		req.user = user;
 		next();
